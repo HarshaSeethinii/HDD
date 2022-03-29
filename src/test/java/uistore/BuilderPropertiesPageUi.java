8c375dc575345ac6a34819bffec70b2e3d23f655
HarshaSeethinii <101277398+HarshaSeethinii@users.noreply.github.com>
2022-03-29 17:54:41 +0530

    Delete src/test directory

--- src/test/java/uistore/BuilderPropertiesPageUi.java
+++
@@ -1,8 +0,0 @@
-package uistore;
-
-import org.openqa.selenium.By;
-
-public class BuilderPropertiesPageUi {
-	static public By resultsHeading = By.xpath("//h1[@class='heading-5 m-0 nb__2HfkL']");
-
-}
--- src/test/java/uistore/SearchResultPageUi.java
+++
@@ -1,7 +0,0 @@
-package uistore;
-
-import org.openqa.selenium.By;
-
-public class SearchResultPageUi {
-	static public By totalResults = By.id("propertyCount");
-}
